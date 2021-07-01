c19532d922ba1898875838079124fc28bae8b7ad
JustinZhang <justinz@andrew.cmu.edu>
2021-07-01 02:18:31 -0400

    Installed basic pug templates. For future reference ctrl + c to kill server, as aparently server is only ended with a SIGKILL signal

--- index.js
+++ index.js
@@ -19,13 +19,16 @@ const port = process.env.PORT || "8000"; //defaults to 8000
 APP CONFIGURATION
 **/
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
 /** 
 ROUTES DEFINITION
 **/
 
-app.get("/", (req,res) => {
-    res.status(200).send("This reply is a response to a GET request!");
-});
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+  });
 
 
 /** 
